c8901b8637061824e286001c8359c0d32b308e15
ijax1 <ianshimjackson@gmail.com>
2022-12-12 14:41:36 -0500

    fix opmode clash, hardware map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OutreachTeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OutreachTeleOpTest.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="TeleOp: Test", group="Tests")
+@TeleOp(name="Outreach TeleOp: Test", group="Tests")
 public class OutreachTeleOpTest extends LinearOpMode {
 
     //Declaring motors here
